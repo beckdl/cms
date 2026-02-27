3fc5db3ef74c0bfe4c7a3c95f3da308c86dd7879
beckdl <icefire24@hotmail.com>
2026-02-27 14:42:25 -0700

    Finished Contacts Edit

--- src/app/contacts/contact-edit/contact-edit.component.ts
+++ src/app/contacts/contact-edit/contact-edit.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ContactService } from '../contact.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Contact } from '../contact.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-edit',
   templateUrl: './contact-edit.component.html',
   styleUrl: './contact-edit.component.css'
 })
-export class ContactEditComponent {
+export class ContactEditComponent implements OnInit {
+  originalContact: Contact;
+  contact: Contact;
+  groupContacts: Contact[] = [];
+  editMode: boolean = false;
+  id: string;
+
+  constructor(private contactService: ContactService, private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.params.subscribe((params) => {
+      this.id = params['id'];
+      if (!this.id) {
+        this.editMode = false;
+        return;
+      }
+      this.originalContact = this.contactService.getContact(this.id);
+      if (!this.originalContact) {
+        this.editMode = false;
+        return;
+      }
+      this.editMode = true;
+      this.contact = JSON.parse(JSON.stringify(this.originalContact));
+      if (this.originalContact.group) {
+        this.groupContacts = JSON.parse(JSON.stringify(this.originalContact.group));
+      }
+    });
+  }
+
+  onSubmit(f: NgForm) {
+    const value = f.value;
+    const newContact = new Contact("", value.name, value.email, value.phone, value.imageUrl, this.groupContacts);
+    if (this.editMode) {
+      this.contactService.updateContact(this.originalContact, newContact);
+    } else {
+      this.contactService.addContact(newContact);
+    }
+    this.router.navigate(['/contacts']);
+  }
+
+  onCancel() {
+    this.router.navigate(['/contacts']);
+  }
 
 }
